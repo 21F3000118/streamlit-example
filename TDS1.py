1f9b954d60edc995f393b78380f238b992cfa6fa
21F3000118 <143696272+21F3000118@users.noreply.github.com>
2023-08-31 22:37:47 +0530

    Update TDS1.py

--- TDS1.py
+++ TDS1.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-def largest_number(a, b, c):
+def find_largest_number(a, b, c):
     return max(a, b, c)
 
 def main():
     st.title("Find the Largest Number")
-    
-    st.write("Enter three numbers:")
-    a = st.number_input("Enter the first number:", value=0.0)
-    b = st.number_input("Enter the second number:", value=0.0)
-    c = st.number_input("Enter the third number:", value=0.0)
-    
-    result = largest_number(a, b, c)
-    st.write(f"The largest number among {a}, {b}, and {c} is: {result}")
+
+    a = st.number_input("Enter the first number:", value=0.0, format="%.1f")
+    b = st.number_input("Enter the second number:", value=0.0, format="%.1f")
+    c = st.number_input("Enter the third number:", value=0.0, format="%.1f")
+
+    if st.button("Find Largest"):
+        largest = find_largest_number(a, b, c)
+        st.success(f"The largest number is: {largest}")
 
 if __name__ == "__main__":
     main()
