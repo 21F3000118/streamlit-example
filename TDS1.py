1706be557b78fae096bbb4dfc0fc7888a8e2b1de
21F3000118 <143696272+21F3000118@users.noreply.github.com>
2023-08-31 22:23:14 +0530

    Update TDS1.py

--- TDS1.py
+++ TDS1.py
@@ -7,9 +7,9 @@ def main():
     st.title("Find the Largest Number")
     
     st.write("Enter three numbers:")
-    a = st.number_input("Enter the first number:", value=0)
-    b = st.number_input("Enter the second number:", value=0)
-    c = st.number_input("Enter the third number:", value=0)
+    a = st.number_input("Enter the first number:", value=0.0)
+    b = st.number_input("Enter the second number:", value=0.0)
+    c = st.number_input("Enter the third number:", value=0.0)
     
     result = largest_number(a, b, c)
     st.write(f"The largest number among {a}, {b}, and {c} is: {result}")
